b2f5d1bd68301a4e0bde05a78962705814a779fe
Wolfgang Beer <wolfgang@smartlab.at>
2025-11-08 10:01:01 +0100

    first version

--- app.py
+++ app.py
@@ -14,6 +14,34 @@ NS_STR = os.environ.get('SETTINGS_SCHEMAS')
 # Split the comma separated string into a list
 NAMESPACES = NS_STR.split(',') if NS_STR else []
 
+def check(items):
+    now = datetime.now().timestamp()
+    for item in items:
+        if item['modified'] / 1000 < now - (86400 * KEEP_PERIOD_DAYS): # item older than X days, delete it
+            dr = requests.delete(YOUR_DT_API_URL + '/api/v2/settings/objects/' + item['objectId'], headers={'Authorization' : 'Api-Token ' + YOUR_DT_API_TOKEN})
+            if dr.status_code == 204:
+                print("Successfully removed settings object with id: %s" % (item['objectId']))
+        
+def cleanup():
+    for ns in NAMESPACES:
+        print("Start cleaning up namespace: %s" % (ns))
+        count = 0
+        r = requests.get(YOUR_DT_API_URL + '/api/v2/settings/objects?fields=objectId,modified&schemaIds=' + ns, headers={'Authorization' : 'Api-Token ' + YOUR_DT_API_TOKEN})
+        if r.status_code == 200: 
+            count = count + len(r.json()['items'])
+            check(r.json()['items'])  
+            while count < r.json()['totalCount']:
+                r = requests.get(YOUR_DT_API_URL + '/api/v2/settings/objects?nextPageKey=' + r.json()['nextPageKey'], headers={'Authorization' : 'Api-Token ' + YOUR_DT_API_TOKEN})
+                if r.status_code == 200: 
+                    count = count + len(r.json()['items'])
+                    check(r.json()['items'])
+                else:
+                    print("Http error: %d" % (r.status_code))
+                    break
+        else:
+            print("Http error: %d" % (r.status_code))
+
+
 
 # Start a simple webserver to receive a http request to trigger the job
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -24,7 +52,8 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Cleanup service")
+        cleanup()
+        self.wfile.write(b"Finished cleanup service")
 
 PORT = 8080
 with HTTPServer(("127.0.0.1", PORT), SimpleHandler) as server:
@@ -35,30 +64,5 @@ if not YOUR_DT_API_URL or not YOUR_DT_API_TOKEN or not NAMESPACES:
     print("Please set the necessary environment variables: KEEP_PERIOD_DAYS, DYNATRACE_API_TOKEN, DYNATRACE_ENVIRONMENT_URL, SETTINGS_SCHEMAS")
     exit(1)
 
-now = datetime.now().timestamp()
-def check(items):
-    for item in items:
-        if item['modified'] / 1000 < now - (86400 * KEEP_PERIOD_DAYS): # item older than X days, delete it
-            dr = requests.delete(YOUR_DT_API_URL + '/api/v2/settings/objects/' + item['objectId'], headers={'Authorization' : 'Api-Token ' + YOUR_DT_API_TOKEN})
-            if dr.status_code == 204:
-                print("Successfully removed settings object with id: %s" % (item['objectId']))
-        
 
-for ns in NAMESPACES:
-    print("Start cleaning up namespace: %s" % (ns))
-    count = 0
-    r = requests.get(YOUR_DT_API_URL + '/api/v2/settings/objects?fields=objectId,modified&schemaIds=' + ns, headers={'Authorization' : 'Api-Token ' + YOUR_DT_API_TOKEN})
-    if r.status_code == 200: 
-        count = count + len(r.json()['items'])
-        check(r.json()['items'])  
-        while count < r.json()['totalCount']:
-            r = requests.get(YOUR_DT_API_URL + '/api/v2/settings/objects?nextPageKey=' + r.json()['nextPageKey'], headers={'Authorization' : 'Api-Token ' + YOUR_DT_API_TOKEN})
-            if r.status_code == 200: 
-                count = count + len(r.json()['items'])
-                check(r.json()['items'])
-            else:
-                print("Http error: %d" % (r.status_code))
-                break
-    else:
-        print("Http error: %d" % (r.status_code))
 
